0524d8f6e6e667e81e530fce7544c247617a96ed
danugans <mas.danu29@gmail.com>
2022-03-04 14:57:36 +0700

    update

--- src/screens/listSoc.js
+++ src/screens/listSoc.js
@@ -4,19 +4,27 @@ import { Text, Box, FlatList, Pressable } from "native-base";
 // Add Props in Hello({navigation})
 export default function Hello({ navigation }) {
   // Set Dummy Data with Array
+  const socialMedia = ["Instagram", "Facebook", "Tiktok", "Twitter"];
 
   // Make Function handle press to get value per list
+  const handlePress = (value) => {
+    navigation.navigate("Detail Social", { value });
+  };
 
   return (
-    <Box
-      safeArea
-      bg="primary.400"
-      flex={1}
-      alignItems="center"
-      justifyContent="center"
-      p={10}
-    >
+    <Box safeArea bg="primary.400" flex={1} alignItems="center" justifyContent="center" p={10}>
       {/* Render Array With Flatlist */}
+      <FlatList
+        data={socialMedia}
+        renderItem={({ item }) => (
+          <Pressable onPress={() => handlePress(item)}>
+            <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30} margin={5}>
+              {item}
+            </Text>
+          </Pressable>
+        )}
+        keyExtractor={(item) => item}
+      />
     </Box>
   );
 }
